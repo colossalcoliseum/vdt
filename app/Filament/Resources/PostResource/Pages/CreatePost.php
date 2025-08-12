a6492183128fdf130f102e83be071cba0711b712
theitalian <0theitalian@gmail.com>
2025-08-11 23:20:31 -0700

    minor additions and/or changes

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -3,10 +3,43 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use App\Models\Post;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Forms\Components\Wizard\Step;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Success')
+            ->body('Post Created');
+    }
+
+    protected function beforeCreate(): void
+    {
+        if (!auth()->user()->can('create', Post::class)) {
+            Notification::make()
+                ->warning()
+                ->title('Warning')
+                ->body('Missing permission to create post!')
+                ->persistent()
+                ->icon('icon-person-exclamation')
+                ->send();
+        }
+        $this->halt();
+    }
 }
--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -3,12 +3,30 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use App\Models\Post;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 class ListPosts extends ListRecords
 {
     protected static string $resource = PostResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'all'=>Tab::make()->icon('icon-house')->badge(Post::all()->count()),
+            'public'=>Tab::make()
+            ->modifyQueryUsing(fn(Builder $query)=> $query->where('visibility', 'public'))
+                ->icon('icon-collection')
+                ->badge(Post::query()->where('visibility','public')->count()),
+            'private'=>Tab::make()
+            ->modifyQueryUsing(fn(Builder $query)=> $query->where('visibility', 'private'))
+            ->icon('icon-lock')
+                ->badge(Post::query()->where('visibility','private')->count()),
+
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
